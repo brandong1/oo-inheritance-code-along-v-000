fa9d342b83fcee05620351062c3df82b9b69e2e1
Brandon Green <brandon@brandonleeproductions.com>
2018-06-25 21:34:10 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,4 +2,7 @@ require_relative "./vehicle.rb"
 
 class Car < Vehicle # We use the < to inherit the Car class from Vehicle. Notice that there are no methods defined in the Car class.
 
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
 end
